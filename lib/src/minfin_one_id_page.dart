43a96851c2727c75bd9bceb0653ae3d3bd07dc9d
abbos2101 <abbos2101@gmail.com>
2022-11-17 17:42:25 +0500

    upgrade version

--- lib/src/minfin_one_id_page.dart
+++ lib/src/minfin_one_id_page.dart
@@ -1,136 +1,120 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:flutter_easyloading/flutter_easyloading.dart';
-// import 'package:webview_flutter/webview_flutter.dart';
-// import 'bloc/login_bloc.dart';
-// import 'minfin_api.dart';
-//
-// class MinfinOneIDPage extends StatefulWidget {
-//   final String clientId;
-//   final String redirectUri;
-//   final String clientSecret;
-//   final String scope;
-//   final String state;
-//   final Widget? background;
-//   final Function(dynamic data)? onGetData;
-//
-//   const MinfinOneIDPage({
-//     Key? key,
-//     required this.clientId,
-//     required this.redirectUri,
-//     required this.clientSecret,
-//     required this.scope,
-//     required this.state,
-//     this.background,
-//     this.onGetData,
-//   }) : super(key: key);
-//
-//   @override
-//   State<MinfinOneIDPage> createState() => _MinfinOneIDPageState();
-// }
-//
-// class _MinfinOneIDPageState extends State<MinfinOneIDPage> {
-//   late final bloc = LoginBloc(MinfinApi(
-//     clientId: widget.clientId,
-//     redirectUri: widget.redirectUri,
-//     clientSecret: widget.clientSecret,
-//     scope: widget.scope,
-//     state: widget.state,
-//   ))
-//     ..add(LoginEvent.init());
-//   late final WebViewController controller;
-//
-//   @override
-//   void dispose() {
-//     bloc.close();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocProvider.value(
-//       value: bloc,
-//       child: BlocConsumer<LoginBloc, LoginState>(
-//         listener: (context, state) async {
-//           if (state.status == EnumStatus.loading) {
-//             EasyLoading.show();
-//           } else if (state.status == EnumStatus.fail) {
-//             EasyLoading.showError(state.message);
-//             if (mounted) Navigator.pop(context);
-//           } else if (state.status == EnumStatus.success) {
-//             widget.onGetData?.call(state.user);
-//           } else if (state.status == EnumStatus.next) {
-//             EasyLoading.dismiss();
-//           }
-//         },
-//         builder: (context, state) {
-//           return SafeArea(
-//             child: Scaffold(
-//               body: Column(
-//                 children: [
-//                   state.progress == 100
-//                       ? const SizedBox()
-//                       : LinearProgressIndicator(
-//                           value: state.progress / 100,
-//                           backgroundColor: Colors.grey[200],
-//                           color: Colors.grey,
-//                           minHeight: 8,
-//                         ),
-//                   Expanded(
-//                     child: Builder(
-//                       builder: (context) {
-//                         if (state.initialUrl.isEmpty) return const SizedBox();
-//                         if (state.status == EnumStatus.next &&
-//                             widget.background != null) {
-//                           return widget.background!;
-//                         }
-//                         return WebView(
-//                           initialUrl: state.initialUrl,
-//                           javascriptMode: JavascriptMode.unrestricted,
-//                           onWebViewCreated: (ctrl) async {
-//                             controller = ctrl;
-//                             await controller.clearCache();
-//                           },
-//                           onProgress: (value) async {
-//                             if (!mounted) return;
-//                             bloc.add(LoginEvent.changed(progress: value));
-//                             final url = await controller.currentUrl();
-//                             if (state.currentUrl != url) {
-//                               bloc.add(LoginEvent.changed(currentUrl: url));
-//                             }
-//                             if (kDebugMode) {
-//                               print("*** Url ***\n$url");
-//                             }
-//                           },
-//                           onWebResourceError: (error) {
-//                             if (!mounted) return;
-//                             bloc.add(LoginEvent.changed(progress: 100));
-//                             if (kDebugMode) {
-//                               print("Handle your Error Page here");
-//                             }
-//                           },
-//                         );
-//                       },
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           );
-//         },
-//       ),
-//     )
-
-//   }
-// }
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'minfin_api.dart';
 
-class MinfinPage extends StatelessWidget {
-  const MinfinPage({Key? key}) : super(key: key);
+class MinfinOneIDPage extends StatefulWidget {
+  final String clientId;
+  final String redirectUri;
+  final String clientSecret;
+  final String scope;
+  final String state;
+  final Function(String url)? onChangeUrl;
+  final Function()? onLoading;
+  final Function(dynamic error)? onError;
+  final Function(dynamic data)? onSuccess;
+
+  const MinfinOneIDPage({
+    Key? key,
+    required this.clientId,
+    required this.redirectUri,
+    required this.clientSecret,
+    required this.scope,
+    required this.state,
+    this.onChangeUrl,
+    this.onLoading,
+    this.onError,
+    this.onSuccess,
+  }) : super(key: key);
+
+  @override
+  State<MinfinOneIDPage> createState() => _MinfinOneIDPageState();
+}
+
+class _MinfinOneIDPageState extends State<MinfinOneIDPage> {
+  late final api = MinfinApi(
+    clientId: widget.clientId,
+    redirectUri: widget.redirectUri,
+    clientSecret: widget.clientSecret,
+    scope: widget.scope,
+    state: widget.state,
+  );
+  late final WebViewController controller;
+  var progress = 0;
+  var initialUrl = "";
+  var currentUrl = "";
+
+  @override
+  void initState() {
+    Future(() => setState(() => initialUrl = api.initialUrl));
+    super.initState();
+  }
+
+  void changedUrl() async {
+    if (currentUrl.contains("http://") || currentUrl.contains("https://")) {
+      const key = "code=";
+      final index = currentUrl.indexOf(key);
+      if (index != -1) {
+        widget.onLoading?.call();
+        final code = currentUrl.substring(
+          index + key.length,
+          currentUrl.indexOf("&"),
+        );
+        try {
+          final token = await api.token(code);
+          final user = await api.getUser(token);
+          await api.logout(token);
+          widget.onSuccess?.call(user);
+        } catch (e) {
+          widget.onError?.call(e);
+        }
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            progress == 100
+                ? const SizedBox()
+                : LinearProgressIndicator(
+                    value: progress / 100,
+                    backgroundColor: Colors.grey[200],
+                    color: Colors.grey,
+                    minHeight: 8,
+                  ),
+            Expanded(
+              child: initialUrl.isEmpty
+                  ? const SizedBox()
+                  : WebView(
+                      initialUrl: initialUrl,
+                      javascriptMode: JavascriptMode.unrestricted,
+                      onWebViewCreated: (ctrl) async {
+                        controller = ctrl;
+                        await controller.clearCache();
+                      },
+                      onProgress: (value) async {
+                        if (!mounted) return;
+                        setState(() => progress = value);
+                        final url = await controller.currentUrl();
+                        if (currentUrl != url) {
+                          setState(() => currentUrl = url ?? "");
+                          changedUrl();
+                          widget.onChangeUrl?.call(url ?? "");
+                        }
+                      },
+                      onWebResourceError: (error) {
+                        if (!mounted) return;
+                        setState(() => progress = 100);
+                        widget.onError?.call(error);
+                      },
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
